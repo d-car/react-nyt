88a5b1bcf05e94b60da4f708f31966adf3ff7bd6
unknown <danie@Dan-Laptop>
2018-10-17 16:36:23 -0500

    controller and routes

---
+++ backend/controllers/api-routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const Article = require(path.join(__dirname, "..", "models", "Article.js"));
+
+
+router.get("/saved", (req, res) => {
+    // Find saved articles
+    Article.find({}, (err, doc) => {
+        if (err) throw err;
+
+        res.send(doc);
+        
+    });
+});
+
+router.post("/saved", (req, res) => {
+    Article.findOne({"id": req.body.article.id}, (err, doc) => {
+        if (err) throw err;
+
+        // Save the article to database if it does not exist yet
+        if (!doc) {
+            // Add a timestamp
+            req.body.article.dateSaved = new Date();
+            
+            const article = new Article(req.body.article);
+
+            article.save((err1, doc1) => {
+                if (err1) throw err1;
+
+                res.send(doc1);
+                
+            });
+
+        } else {
+            res.status(409).send("You already saved this article.");
+
+        }
+
+    });
+});
+
+router.delete("/saved", (req, res) => {
+    Article.remove({"id": req.body.id}, (err, doc) => {
+        if (err) throw err;
+
+        res.send(doc);
+
+    });
+});
+
+
+module.exports = router;
